f86fb6232d5718b61fcd4a1930f457337512636e
Hal DeLage <hal.delage@discoverorg.com>
2018-07-11 11:35:49 -0700

    cleanup

--- healthchecker/healthchecker.js
+++ healthchecker/healthchecker.js
@@ -1,10 +1,7 @@
 const http = require('http');
 const cp = require('child_process');
 const process = require('process');
-//const util = require('util');
 const logger = require('./logs');
-
-//const env = process.env.NODE_ENV || 'development';
 
 const testInterval = 30000;   //how often test will be run
 const statusPort = process.env.PORT || 3000;      //webserver is listening on this port
